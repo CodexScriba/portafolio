af4e5acf84eb379f256cbc1b6e5de5f5e52b7475
CodexScriba <dev.daniel.garcia@gmail.com>
2024-07-26 05:35:24 -0600

    In progress - Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import Hero from '@/components/Hero'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <main className='relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
+      <div className='max-w-7xl w-full'>
+        <Hero /> 
+      </div>
+    </main>
   )
 }
 
