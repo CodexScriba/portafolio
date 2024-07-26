70eea022f1c4aa571f2cd98e5180e6c16ff1616a
daniel0123456789 <daniel0123456789@gmail.com>
2024-07-26 21:12:11 +0000

    install - added Aceternity ui and framer motion.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Spotlight } from './ui/Spotlight'
 
 const Hero = () => {
   return (
-    <div className=' text-white'>Hero</div>
+    <div className=' pb-20 pt-36 '>
+      <div>
+        <Spotlight className= "-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white"/>
+        <Spotlight className= "-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white"/>
+        <Spotlight className= "-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white"/>
+      </div>
+    </div>
   )
 }
 
