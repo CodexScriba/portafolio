055289f19bf17ff778886fb6ed375592e1d630e7
daniel0123456789 <daniel0123456789@gmail.com>
2024-07-30 07:46:13 +0000

    added dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,16 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import type { ReactNode } from "react";
+import { ThemeProvider } from "./provider";
 
 const poppins = Poppins({
   weight: ['400', '700'],  // You can adjust weights as needed
   subsets: ['latin'],
   display: 'swap',
 });
+
 export const metadata: Metadata = {
-  title: "Daniel's Portafolio",
+  title: "Daniel's Portfolio",
   description: "Modern minimalistic website with a slick design",
 };
 
@@ -19,8 +21,18 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
-      <body className={poppins.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body className={poppins.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
